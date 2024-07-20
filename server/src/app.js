24fa0b3061bc5b0273f388b1863235b9188f2530
dilpreet <oracussiter@gmail.com>
2024-07-20 09:53:20 +0530

    removed faulty middleware

--- server/src/app.js
+++ server/src/app.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json({limit : '17kb'}))
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use()
+
 
 export const userSocketIDs = new Map();
 export const onlineUsers = new Set();
